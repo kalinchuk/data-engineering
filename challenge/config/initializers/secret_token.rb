af42afd0c6d7843b3a9b41dc8f1ecad5e8c3dced
Artem Kalinchuk <artem.kalinchuk@visaops.com>
2014-04-02 17:00:39 -0400

    Blank app with some extra gems

---
+++ challenge/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge::Application.config.secret_key_base = '5d9160ba72637a138da33d3682fa79cdbe838e85630760d30f626d109921b3a8bb0bf29339889e255257e49cd3bba251e39442a22a614c352a2bcbbf064849df'
