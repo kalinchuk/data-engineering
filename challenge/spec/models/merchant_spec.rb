349d7df1b1f4254ea740b23e27b59baf80b5df3b
Artem Kalinchuk <artem.kalinchuk@visaops.com>
2014-04-02 18:01:06 -0400

    Update Merchant specs.

--- challenge/spec/models/merchant_spec.rb
+++ challenge/spec/models/merchant_spec.rb
@@ -9,8 +9,41 @@
 #  updated_at :datetime
 #
 
-require 'spec_helper'
-
 describe Merchant do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    let(:creation_attributes) {{
+      name: Faker::Name.name,
+      address: Faker::Address.street_address
+    }}
+
+    subject { Merchant.create(creation_attributes) }
+
+    describe "validations" do
+      it "accepts a merchant with all attributes" do
+        expect(subject).to be_valid
+      end
+
+      it "rejects a merchant without a name" do
+        creation_attributes.delete :name
+        expect(subject).not_to be_valid
+      end
+
+      it "rejects a merchant without an address" do
+        creation_attributes.delete :address
+        expect(subject).not_to be_valid
+      end
+
+      it "accepts a merchant from a factory" do
+        expect(create(:merchant)).to be_valid
+      end
+    end
+
+    describe "associations" do
+      it "has many items" do
+        item = create(:item, merchant: subject)
+        expect(subject.items).to include item
+        expect(item.merchant).to eq subject
+      end
+    end
+  end
 end
