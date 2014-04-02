88b283e5bf6f91e0e6a7a2bd6aabd5f7ab363c4c
Artem Kalinchuk <artem.kalinchuk@visaops.com>
2014-04-02 17:55:06 -0400

    Update Item specs.

--- challenge/spec/models/item_spec.rb
+++ challenge/spec/models/item_spec.rb
@@ -9,8 +9,46 @@
 #  updated_at  :datetime
 #
 
-require 'spec_helper'
-
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    let(:merchant) { create(:merchant) }
+    let(:creation_attributes) {{
+      merchant_id: merchant.id,
+      description: Faker::Lorem.paragraph,
+      price: 55.56
+    }}
+
+    subject { Item.create(creation_attributes) }
+
+    describe "validations" do
+      it "accepts an item with all attributes" do
+        expect(subject).to be_valid
+      end
+
+      it "rejects an item without a description" do
+        creation_attributes.delete :description
+        expect(subject).not_to be_valid
+      end
+
+      it "rejects an item without a price" do
+        creation_attributes.delete :price
+        expect(subject).not_to be_valid
+      end
+
+      it "rejects an item with a price at or less than 0" do
+        creation_attributes[:price] = 0
+        expect(subject).not_to be_valid
+      end
+
+      it "accepts an item from a factory" do
+        expect(create(:item)).to be_valid
+      end
+    end
+
+    describe "associations" do
+      it "is associated with the merchant" do
+        expect(subject.merchant).to eq merchant
+      end
+    end
+  end
 end
